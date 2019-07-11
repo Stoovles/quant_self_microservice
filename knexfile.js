f5817aeee1afc9fccf40693a91fd86b0050569cc
Ethan Grab <ethangrab@gmail.com>
2019-07-10 22:18:43 -0600

    knex DB config

---
+++ knexfile.js
@@ -0,0 +1,65 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'ethangrab',
+      password : null,
+      database : 'qs_microservice',
+      charset: 'utf8'
+    },
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+
+  staging: {
+    client: 'pg',
+    connection: {
+      database: 'qs_microservice_staging',
+      user:     'ethangrab',
+      password: null
+    },
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    }
+  },
+
+  production: {
+    client: 'pg',
+
+    connection: process.env.DATABASE_URL,
+
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    }
+  },
+
+  test: {
+    client: 'pg',
+    connection: {
+      database: 'qs_microservice_test',
+      user: 'ethangrab',
+      password: null
+    },
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    }
+  }
+};
